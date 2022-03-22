20b3c81b1eec66a8b0dc6ec3c1805295a9655b61
Ваше Имя <>
2022-03-22 16:39:55 +0300

    Init commit

---
+++ Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('moduleexample::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('moduleexample.name') !!}
+    </p>
+@endsection
