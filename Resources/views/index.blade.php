25c77f9185cb8d5ca52e6fefeca6fdc215bd7b80
Ваше Имя <>
2022-03-24 14:03:23 +0300

    added admnlte

--- Resources/views/index.blade.php
+++ Resources/views/index.blade.php
@@ -1,9 +1,18 @@
-@extends('moduleexample::layouts.master')
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
 
 @section('content')
-    <h1>Hello World</h1>
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
 
-    <p>
-        This view is loaded from module: {!! config('moduleexample.name') !!}
-    </p>
-@endsection
+@section('css')
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
